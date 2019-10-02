ca1788c220031dd047bba57bfe15ad7c8a1c83c0
Armitroner <alex.ny@hotmail.ca>
2019-10-01 23:20:55 -0400

    Added Project
    AddedProject. Also screw 3d game kit.

---
+++ Assets/RequiredFiles/PlayMaker/Editor/PlayMakerBuildCallbacks.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using HutongGames.PlayMaker;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace HutongGames.PlayMakerEditor
+{
+    public class PlayMakerBuildCallbacks
+    {
+        [PostProcessSceneAttribute(2)]
+        public static void OnPostprocessScene()
+        {
+            /* TODO: Figure out if we need to do this!
+            // OnPostprocessScene is called when loading a scene in the editor 
+            // Might not want to post process in that case...?
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                return;
+            }*/
+
+            //Debug.Log("OnPostprocessScene");
+
+            if (Application.isPlaying) // playing in editor, not really making a build
+            {
+                return;
+            }
+
+            PlayMakerGlobals.IsBuilding = true;
+            PlayMakerGlobals.InitApplicationFlags();
+
+            var fsmList = Resources.FindObjectsOfTypeAll<PlayMakerFSM>();
+            foreach (var playMakerFSM in fsmList)
+            {
+                //Debug.Log(FsmEditorUtility.GetFullFsmLabel(playMakerFSM));
+                
+                // TODO: we need a new build tool to process prefabs
+                if (FsmPrefabs.IsPrefab(playMakerFSM.Fsm)) continue;
+                
+                playMakerFSM.Preprocess();
+            }
+
+            PlayMakerGlobals.IsBuilding = false;
+
+            //Debug.Log("EndPostProcessScene");
+        }
+    }
+}
